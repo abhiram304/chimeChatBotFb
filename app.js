068c8e4a74fa6a90476aa05ca196fa11bf138336
abhiram304 <abhiram.304@gmail.com>
2017-06-11 04:48:04 -0700

    first commit

--- app.js
+++ app.js
@@ -1,120 +1,294 @@
-var express           =     require('express')
-  , passport          =     require('passport')
-  , util              =     require('util')
-  , FacebookStrategy  =     require('passport-facebook').Strategy
-  , session           =     require('express-session')
-  , cookieParser      =     require('cookie-parser')
-  , bodyParser        =     require('body-parser')
-  , config            =     require('./configuration/config')
-  , mysql             =     require('mysql')
-  , app               =     express();
 
-//Define MySQL parameter in Config.js file.
-var connection = mysql.createConnection({
-  host     : config.host,
-  user     : config.username,
-  password : config.password,
-  database : config.database
-});
-//EAAYAllYwB8EBAGpdHGX0d5oyTTC5fJnbnh15ck1ZBUgGJrD45cVfkhPVcSJbrJcO59YsVpvXhRkQ3tdap39D2g2OZBxmZBpQz7P8Pt1hjDYrFIIknjyHDCM9coMDJZBlFrsKZCHinln4nIx5ZBNerjOVXep8S9ZCIZBk9LROgZAMt7gZDZD
-//Connect to Database only if Config.js parameter is set.
+/**
+ * Module dependencies.
+ */
 
-if(config.use_database==='true')
-{
-    connection.connect();
-}
+var express = require('express')
+, routes = require('./routes')
+, user = require('./routes/user')
+, http = require('http')
+, path = require('path');
+var request = require('request');
+var app = express();
 
-// Passport session setup.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-
-// Use the FacebookStrategy within Passport.
-
-passport.use(new FacebookStrategy({
-    clientID: config.facebook_api_key,
-    clientSecret:config.facebook_api_secret ,
-    callbackURL: config.callback_url,
-    profileFields: ['id', 'emails', 'name']
-  },
-  function(accessToken, refreshToken, profile, done) {
-	  console.log("passport used ="+profile.emails[0].value);
-    process.nextTick(function () {
-      //Check whether the User exists or not using profile.id
-      console.log("Details: "+profile.id);
-    	if(config.use_database==='true')
-      {
-      connection.query("SELECT * from user where fbid="+profile.id,function(err,rows,fields){
-        if(err) throw err;
-        if(rows.length===0)
-          {
-            console.log("There is no such user, adding now "+ JSON.stringify(profile));
-            connection.query("INSERT into user(name, fbid, email) VALUES('"+profile.name.givenName+"','"+profile.id+"', '"+profile.emails[0].value+"')");
-          }
-          else
-            {
-              console.log("User already exists in database");
-            }
-          });
-      }
-      return done(null, profile);
-    });
-  }
-));
-
-
+//all environments
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({ secret: 'working secret', key: 'sid'}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.static(__dirname + '/public'));
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-  res.render('index', { user: req.user });
-});
-//Verify fb messnger
-app.get('/webhook', function(req, res) {
-	  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'chime') {
-	    res.status(200).send(req.query['hub.challenge']);
-	  } else {
-	    res.status(403).end();
-	  }
-	});
-//EAAGCu5vOWZA8BACWsfqfZCz6ZCjBZAFwpfkMF8zfDqNrBwnGqpUunHFeBQFdEPFS20gnsQjYvHkm2E7AR4d0VLE25PHElmFXZBYJqUXQm1L9izZCJIod4hQOrrbF7mSLzL7RCqClVwSumZAqV1EIBSxOcjZBVfjHMnrF5vFrH0aodQZDZD
-//Post messages
-
-app.get('/account', ensureAuthenticated, function(req, res){
-  res.render('account', { user: req.user });
-});
-
-
-app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
-app.get('/connect/facebook', passport.authorize('facebook', { scope : ['email'] }));
-
-app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect : '/', failureRedirect: '/login' }),
-  function(req, res) {
-	console.log("In the redirect:");
-    res.redirect('/');
-  });
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+//development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
 }
 
-app.listen(3000);
+app.get('/', routes.index);
+app.get('/users', user.list);
+//Verify fb messnger
+app.get('/webhook', function(req, res) {
+	if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'chime') {
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		res.status(403).end();
+	}
+});
+//Post messages
+app.post('/webhook', function(req, res)  {
+	console.log(req.body);
+	if (req.body.object === 'page') {
+		req.body.entry.forEach(function(entry) {
+			entry.messaging.forEach(function(event){
+				if (event.message && event.message.text) {
+					validateMessage(event);
+					//sendMessage(event);
+				}
+			});
+		});
+		res.status(200).end();
+	}
+});
+function validateMessage(event){
+	var sender = event.sender.id;
+	var text = event.message.text;
+	var reply = "I couldn't understand";
+	var sender = event.sender.id;
+
+
+	qArray = ["Do you like math?", "Do you know what 3x4 is?", "How about, if you made 10 Syrian Pounds, what percentage would you have if you bought a 2 pound 50 candy?", "What about stories? Do you like to read?", "What\'s the third letter of the word \'Power\'?", "What\'s the subject\'s intent in the sentence, \"Mohammed wanted me to come to his room after class\"?", "Do you know what a sanitary pad is?", "How do you put on condoms?", "How do you know if you have an STD?", "How is your Turksish? Can you say, \"What\'s your name\" in Turkish?"];
+	aArray = ["yes", "12", "10", "yes", "w", "Mohammed wanted me", "yes", "", "", ""];
+	//Check text with email of db
+	//if its an email then update the messenger id
+	var queryToCheckEmail = "SELECT * from user where email='"+event.message.text+"'";
+	mysql.fetchData(
+			function(err, results) {
+				if (err) {
+					console.log("Error: Some problem with db"+err);
+					throw err;
+				} else {
+					if(results.length >0){
+						//add messenger_id to the row
+						var updateMessengerId = "Update user set messenger_id='"+sender+"' where email='"+event.message.text+"'";
+						mysql.fetchData(
+								function(err, results) {
+									if (err) {
+										console.log("Error: Some problem with db"+err);
+										throw err;
+									} else {
+										console.log("Messenger id updated");	
+									}
+
+								}, updateMessengerId);
+
+					}
+
+					else{
+						//Check the state count
+						if(results.state_counter<9){
+							//Check answer
+							if(aArray[results.state_counter] ===  text){
+								//Right answer
+								//update the math
+								if(results.state_counter<3){
+									//update maths part
+									var updateMaths = "Update user set math_rating=math_rating+3 where messenger_id='"+sender+"'";
+									mysql.fetchData(
+											function(err, results) {
+												if (err) {
+													console.log("Error: Some problem with db"+err);
+													throw err;
+												} else {
+													console.log("updateMaths rating");	
+
+													var updateStateCounter = "Update user set state_counter=state_counter+1 where messenger_id='"+sender+"'";
+													mysql.fetchData(
+															function(err, results) {
+																if (err) {
+																	console.log("Error: Some problem with db"+err);
+																	throw err;
+																} else {
+																	console.log("state_counter updated");	
+																	reply = qArray[results.state_counter];
+																	sendMessage(event, reply);
+																}
+
+															}, updateStateCounter);
+
+
+
+												}
+
+											}, updateMaths);
+
+								}
+								else if(results.state_counter>=3 && results.state_counter<6){
+									//update verbal part
+									var updateVerbal = "Update user set verbal_rating=math_rating+3 where messenger_id='"+sender+"'";
+									mysql.fetchData(
+											function(err, results) {
+												if (err) {
+													console.log("Error: Some problem with db"+err);
+													throw err;
+												} else {
+													console.log("updateVerbal rating updated");	
+
+													var updateStateCounter = "Update user set state_counter=state_counter+1 where messenger_id='"+sender+"'";
+													mysql.fetchData(
+															function(err, results) {
+																if (err) {
+																	console.log("Error: Some problem with db"+err);
+																	throw err;
+																} else {
+																	console.log("state_counter updated");
+																	reply = qArray[results.state_counter];
+																	sendMessage(event, reply);
+																}
+
+															}, updateStateCounter);
+
+
+
+												}
+
+											}, updateVerbal);
+								}
+							}else{
+								//wrong answer
+							}
+							//question = stateconter
+							reply = qArray[results.state_counter];
+							sendMessage(event, reply);
+						}
+						else{
+							//session not found. Ask user for his email id to continue
+							reply = "You've already attempted all the questions!";	
+							sendMessage(event, reply);
+						}
+
+
+
+					}
+
+					/*else{//Ask user his email id
+						reply = "What is your email?"
+							//answer ="abhiram.304@gmail.com";
+						//Update messenger_id
+						sendMessage(event, reply); 
+					}*/
+
+				} 
+
+			}, queryToCheckEmail);
+
+
+
+
+
+	if(event.message.text == 'hello' || 'whats up' || 'hey'){
+
+
+		//check if session exists in db..i.e check if his msg_id exists in db
+		var queryToCheckIfSessionExist = "SELECT state_counter from user where messenger_id='"+event.sender.id+"'";
+		mysql.fetchData(
+				function(err, results) {
+					if (err) {
+						console.log("Error: Some problem with db"+err);
+						throw err;
+					} else {
+						if(results.length >0){
+							//Email is attached with messenger_id
+
+							if(results.state_counter<9){
+								//question = stateconter
+								reply = qArray[results.state_counter];
+								sendMessage(event, reply);
+							}
+							else{
+								//session not found. Ask user for his email id to continue
+								reply = "You've already attempted all the questions!";	
+								sendMessage(event, reply);
+							}
+
+
+						}else{//Ask user his email id
+							reply = "What is your email?"
+								answer ="abhiram.304@gmail.com";
+							//Update messenger_id
+							sendMessage(event, reply); 
+						}
+
+					}
+
+				}, queryToCheckIfSessionExist);
+
+		//If answered all questions, tell answer that he has already taken this quiz
+		//Else continue from where he paused
+	} 
+	else if(event.message.text == 'bye' || 'good bye'){
+		//check if session exists
+		reply = "bye";
+		sendMessage(event, reply);
+	}
+	else{
+		reply = "I can't understand";
+		sendMessage(event, reply);
+	}
+	//Check if the email id is valid for the first time
+
+}
+/*var questions = ["a", "b",  "];
+
+//If session is found
+function sessionFound(event){
+	//Session found. Check state_counter value
+	var queryToFetchUnansweredQuestions = "SELECT state_counter from user where messenger_id='"+event.sender.id+"'";
+	mysql.fetchData(
+			function(err, results) {
+				if (err) {
+					console.log("Error: Some problem with db"+err);
+					throw err;
+				} else {
+					var questionNumber = results.state_counter;
+					var questionName = "question"+ (questionNumber+1);
+
+				}
+
+			}, queryToFetchUnansweredQuestions);
+
+}
+
+//If there is no session
+function nosessionExist(event){
+
+}*/
+
+function sendMessage(event, reply) {
+
+	var sender = event.sender.id;
+	var text = reply;
+	console.log("SENDER: "+sender);
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token: "EAAGCu5vOWZA8BACWsfqfZCz6ZCjBZAFwpfkMF8zfDqNrBwnGqpUunHFeBQFdEPFS20gnsQjYvHkm2E7AR4d0VLE25PHElmFXZBYJqUXQm1L9izZCJIod4hQOrrbF7mSLzL7RCqClVwSumZAqV1EIBSxOcjZBVfjHMnrF5vFrH0aodQZDZD"},
+		method: 'POST',
+		json: {
+			recipient: {id: sender},
+			message: {text: text}
+		}
+	}, function (error, response) {
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+	});
+}
+
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
