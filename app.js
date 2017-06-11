0a38110657cb7d2164bbcd1940c5ba42e3e1de62
abhiram304 <abhiram.304@gmail.com>
2017-06-11 00:06:15 -0700

    first commit

--- app.js
+++ app.js
@@ -63,7 +63,10 @@ app.post('/webhook', function (req, res) {
 	  }
 	});
 	  
-	
+function receivedMessage(event) {
+	  // Putting a stub for now, we'll expand it in the following steps
+	  console.log("Message data: ", event.message);
+	}	
 
 		http.createServer(app).listen(app.get('port'), function(){
 			console.log('Express server listening on port ' + app.get('port'));
